ea6622f6b6df2c5c476c608abe5a673fc563a789
david <Davidcla@hotmail.com>
2014-10-21 12:13:57 +0200

    added admin borrower

--- LibrarySystem/AdminBorrowers.aspx.cs
+++ LibrarySystem/AdminBorrowers.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using LibrarySystem.DTO;
+using LibrarySystem.BL;
 
 namespace LibrarySystem
 {
@@ -11,7 +13,36 @@ namespace LibrarySystem
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                rptAdminBorrower.DataSource = Borrower.getAll();
+                rptAdminBorrower.DataBind();
+            }
+        }
 
+        protected void AddButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("CreateUser.aspx");
+        }
+
+        protected void Loansbut_Command(object sender, CommandEventArgs e)
+        {
+            Session["rptAdminBorrowers"] = e.CommandArgument;
+            Response.Redirect("ViewLoans.aspx");
+        }
+
+        protected void Editbut_Command(object sender, CommandEventArgs e)
+        {
+
+        }
+
+        protected void PreviousBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void NextBtn_Click(object sender, EventArgs e)
+        {
         }
     }
 }
